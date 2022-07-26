e53e98bd8278e5fbdaf90034aa38ae4d9e859e39
pdhart11 <106359270+pdhart11@users.noreply.github.com>
2022-07-26 18:16:17 -0400

    Create binaryTree.c

---
+++ binaryTree.c
@@ -0,0 +1,16 @@
+
+ struct TreeNode 
+ {
+      int val;
+      struct TreeNode *left;
+      struct TreeNode *right;
+ };
+ 
+bool checkTree(struct TreeNode* root)
+{
+    if (root->left->val + root->right->val == root->val)
+    {
+        return true;
+    }
+    return 0;
+}
